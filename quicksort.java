3535a996e638ae220b4418fbc23d8c94fb5abad5
wklglider <wklglider@gmail.com>
2017-01-01 17:14:50 -0700

    Create quicksort.java

---
+++ quicksort.java
@@ -0,0 +1,18 @@
+public void quicksort(int[] array, int left, int right) {
+	if (left < right) {
+		int pivot = array[right];
+		int wall = left;
+		for (int i = left; i < right; i++) {
+			if (array[i] <= pivot) {
+				int temp = array[wall];
+				array[wall++] = array[i];
+				array[i] = temp;
+			}
+		}
+		int temp = array[wall];
+		array[wall ] = array[right];
+		array[right] = temp;
+		quicksort(array, left, wall - 1);
+		quicksort(array, wall + 1, right);
+	}
+}
