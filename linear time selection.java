bbd61066934811bdd7a51ca276440874d4e8ab3c
wklglider <wklglider@gmail.com>
2017-01-01 22:27:48 -0700

    Update and rename linear time selection - java to linear time selection.java

---
+++ linear time selection.java
@@ -0,0 +1,35 @@
+public int randomizedSelect(int[] array, int left, int right, int index) {
+	if (left == right) {
+		return array[left];
+	}
+	int wall = randomizedPartition(array, left, right);
+	int k = wall - left + 1;
+	if (index == k) {
+		return array[wall];
+	} else if (index < k) {
+		return randomizedSelect(array, left, wall - 1, index);
+	} else {
+		return randomizedSelect(array, wall + 1, right, index - k);
+	}
+}
+
+public int randomizedPartition(int[] array, int left, int right) {
+	int randomPick = ThreadLocalRandom.current().nextInt(left, right + 1);
+	int pivot = array[randomPick];
+	array[randomPick] = array[right];
+	array[right] = pivot;
+	int wall = left;
+	for (int i = left; i < right; i++) {
+		//Comparison happens only between the two parentheses.
+		//Descending order would be (array[i] > pivot).
+		if (array[i] < pivot) {
+			int temp = array[wall];
+			array[wall++] = array[i];
+			array[i] = temp;
+		}
+	}
+	int temp = array[wall];
+	array[wall] = array[right];
+	array[right] = temp;
+	return wall;
+}
